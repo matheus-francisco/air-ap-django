ae7e3f934f3d71374120d39b4efe09343cb1591b
matheusfrancisco <matheusmachadoufsc@gmail.com>
2019-08-11 15:27:25 -0300

    Add docker with postgres and config settings

--- airap/settings.py
+++ airap/settings.py
@@ -76,10 +76,15 @@ WSGI_APPLICATION = 'airap.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+# Not is good pratice using  here de PASSWORD
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'database_airap',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
+        'PORT': 5432
     }
 }
 
