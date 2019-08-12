abcf23c14a6f938e4254def1746096ff79754cf3
matheusfrancisco <matheusmachadoufsc@gmail.com>
2019-08-11 23:50:21 -0300

    Add media link

--- airap/settings.py
+++ airap/settings.py
@@ -131,3 +131,9 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'airap/static')
 ]
+
+# Media Folder Settings
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
