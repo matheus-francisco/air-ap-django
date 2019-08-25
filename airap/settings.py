99112ca4e3f3479cb4133201663b9f5680eb8ab2
matheusfrancisco <matheusmachadoufsc@gmail.com>
2019-08-25 12:46:41 -0300

    Add urls contacts and config email

--- airap/settings.py
+++ airap/settings.py
@@ -147,3 +147,13 @@ from django.contrib.messages import constants as messages
 MESSAGE_TAGS = {
     messages.ERROR: 'danger'
 }
+
+
+# email config
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = ''
+EMAI_HOST_PASSWORD = ''
+EMAIL_USE_TLS=True
+
